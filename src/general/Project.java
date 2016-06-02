96f9af425f72802d5bd15f276a39ef795972ada4
Alex609 <dragon609@yandex.ru>
2016-06-03 01:26:59 +0300

    2_4

--- src/general/Project.java
+++ src/general/Project.java
@@ -12,9 +12,9 @@ import java.util.List;
 
 public class Project {
 
-	public static void main(String[] args) throws SQLException {
+	public static void main(String[] args) throws SQLException, InterruptedException {
 		Factory factory = Factory.getInstance();
-		System.out.println("List");
+		//System.out.println("List");
 		ListOsmotraDao listDao = factory.getListOsmotraDao();
 		ListOsmotra li = new ListOsmotra();
 	//	li.setDiuresisid(12);
@@ -29,19 +29,21 @@ public class Project {
 		
 		for(ListOsmotra l : list){
 			l.getId();
-		System.out.println(l.getDateTime());}
+		//System.out.println(l.getDateTime());
+		}
 		
 		ListOsmotra z = listDao.getListOsmotra(1);
-		System.out.println(z.getId() +"   "+ z.getDiuresisid()+"   "+z.getAd());
+		//System.out.println(z.getId() +"   "+ z.getDiuresisid()+"   "+z.getAd());
 		
 		
 		
-		System.out.println("Zur");
+		//System.out.println("Zur");
 PhoneConsultationsJournalDao zurDao = factory.getPhoneConsultationsJournalDao();
 
 	
 		
-		
+		//PhoneConsultationsJournal j =  phonecollGUI.start();
+		//System.out.println(j.getReason());
 		zurDao.addPhoneConsultationsJournal(phonecollGUI.start());
 		
 		List<PhoneConsultationsJournal> zurnal = zurDao.getPhoneConsultationsJournals();
