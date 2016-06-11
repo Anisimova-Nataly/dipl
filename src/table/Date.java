da3605a37b9b5c639a43a8e1860674aeab79b87c
Alex609 <dragon609@yandex.ru>
2016-06-11 18:38:04 +0300

    исправление имен

---
+++ src/table/Date.java
@@ -0,0 +1,34 @@
+package table;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/*CREATE TABLE date(id integer PRIMARY KEY, previousactivevisit date, call date);*/
+
+@Entity
+@Table(name = "date")
+
+public class Date {
+	@Id	
+	 @GeneratedValue(strategy = GenerationType.IDENTITY)
+	 @Column(name = "id")
+	private Integer id;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	@Column(name="previousactivevisit")
+	private java.util.Date previousactivevisit;
+	public java.util.Date getPreviousactivevisit() {
+		return previousactivevisit;
+	}
+	public void setPreviousactivevisit(java.util.Date previousactivevisit) {
+		this.previousactivevisit = previousactivevisit;
+	}
+}
