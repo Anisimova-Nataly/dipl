11c51786645000d8a757d09d60f4328d56508b05
Alex609 <dragon609@yandex.ru>
2016-06-12 16:33:36 +0300

    добавление таблицы звонившего_1

---
+++ src/dao/CallerDao.java
@@ -0,0 +1,13 @@
+package dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import table.Caller;
+
+public interface CallerDao {
+	public void addCaller(Caller li) throws SQLException;
+	public void deleteCaller(Caller li) throws SQLException;	
+	public Caller getCaller (int id) throws SQLException;
+	public List<Caller> getCaller () throws SQLException;
+}
