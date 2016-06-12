06cbce8e22c93d27ff6e32cf3232128fe81e63b7
Alex609 <dragon609@yandex.ru>
2016-06-12 04:15:53 +0300

    добавление таблицы манипуляции

---
+++ src/dao/ManipulationDao.java
@@ -0,0 +1,13 @@
+package dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import table.Manipulation;
+
+public interface ManipulationDao {
+	public void addManipulation(Manipulation li) throws SQLException;
+	public void deleteManipulation(Manipulation li) throws SQLException;	
+	public Manipulation getManipulation (int id) throws SQLException;
+	public List<Manipulation> getManipulation () throws SQLException;
+}
